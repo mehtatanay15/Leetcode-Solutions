a78254a8c743c334237fbd90ae3b842223d99693
Tanay Mehta <162467566+mehtatanay15@users.noreply.github.com>
2026-04-29 22:45:32 +0530

    Add Merge Two Sorted Lists solution
    Implement mergeTwoLists function to merge two sorted linked lists.

---
+++ leetcode-cpp/LinkedList/Other/Merge_ll.cpp
@@ -0,0 +1,47 @@
+// LeetCode 21 - Merge Two Sorted Lists
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        
+        // Dummy node to simplify merging
+        ListNode* dummy = new ListNode(-1);
+        ListNode* tail = dummy;
+
+        // Compare both lists and attach smaller node
+        while (list1 != nullptr && list2 != nullptr) {
+            
+            if (list1->val <= list2->val) {
+                tail->next = list1;
+                list1 = list1->next;
+            } 
+            else {
+                tail->next = list2;
+                list2 = list2->next;
+            }
+
+            tail = tail->next;
+        }
+
+        // Attach remaining nodes
+        if (list1 != nullptr) {
+            tail->next = list1;
+        } 
+        else {
+            tail->next = list2;
+        }
+
+        return dummy->next;
+    }
+};
